8f0697d0366f80d0dc38754ff01308235a30a3a7
Stjepan Ferber <sf49671@fer.hr>
2021-11-04 16:57:53 +0100

    remove .env

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app= express();
+const https = require('https');
+const fs=require('fs')
+const path=require('path')
 require('dotenv').config();
 const {auth,requiresAuth} = require('express-openid-connect');
 
@@ -17,9 +20,20 @@ app.use(
 app.get('/',(req,res)=>{
     res.send(req.oidc.isAuthenticated() ? 'Logged in':'Logged out')
 });
+if(port==3000){
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
 });
+} else{
+    const sslServer=https.createServer(
+        {
+        key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+        cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
+    },app)
+    sslServer.listen(port,()=>console.log(`ssl server listening on port ${port}`))
+}
+
+
 app.get('/profile',requiresAuth(),(req,res)=> {
     res.send(JSON.stringify(req.oidc.user))
 })
